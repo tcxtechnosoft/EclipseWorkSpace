a68917dd778c4955c3b68e0773fc3395956c6c67
christo <dondass96@gmail.com>
2018-09-30 12:58:33 -0700

    9/30/2018

--- Sample/src/scanner.java
+++ Sample/src/scanner.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 public class scanner {
-int a,b,c;
+int a,b,c,d,e,f;
 Scanner s =new Scanner (System.in);
 void input()
 {
 	System.out.println("enter the a and b value : \n");
 	a =s.nextInt();
 	b =s.nextInt();
-	c =a*b;
-	 System.out.println("your answer "+c);
+	c =a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	 System.out.println("your answer " +c);
+	 System.out.println("your answer " +d);
+	 System.out.println("your answer " +e);
+	 System.out.println("your answer " +f);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
